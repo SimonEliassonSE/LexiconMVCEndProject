466903c34403b40a4a10fbe0bba58c38ccc96b9f
a18ismca <ismetcatovic@hotmail.se>
2022-10-26 16:02:18 +0200

    Added views to Category.

---
+++ LexiconMVCEndProject/Controllers/CategoryController.cs
@@ -0,0 +1,70 @@
+﻿using LexiconMVCEndProject.Data;
+using LexiconMVCEndProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LexiconMVCEndProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var categories = _context.Categories;
+            if(categories != null)
+            {
+                return View(categories);
+            }
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            ModelState.Remove("Products");
+
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+            }
+
+            ViewBag.Statement = "A new category has been added.";
+            return RedirectToAction("Index", ViewBag.Statement);
+        }
+
+       
+
+        public IActionResult Edit(int id)
+        {
+            var category = _context.Categories.FirstOrDefault(x => x.CategoryId == id);
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            _context.Categories.Update(category);
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var category = _context.Categories.FirstOrDefault(x => x.CategoryId == id);
+            return View(category);
+        }
+    }
+}
