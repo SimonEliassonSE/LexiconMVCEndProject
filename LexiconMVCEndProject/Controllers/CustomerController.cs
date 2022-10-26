2219e5fd9d6e454b4c7f7fbbd106fbe0224db4d5
SimonEliassonSE <102283683+SimonEliassonSE@users.noreply.github.com>
2022-10-26 14:34:10 +0200

    Customer Done.

--- LexiconMVCEndProject/Controllers/CustomerController.cs
+++ LexiconMVCEndProject/Controllers/CustomerController.cs
@@ -1,12 +1,131 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LexiconMVCEndProject.Data;
+using LexiconMVCEndProject.Models;
+using LexiconMVCEndProject.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LexiconMVCEndProject.Controllers
 {
     public class CustomerController : Controller
     {
+        readonly ApplicationDbContext _context;
+
+        public CustomerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var customers = _context.Customers.ToList();            
+
+            return View(customers);
+        }
+
+        [HttpGet]
+        public IActionResult AddCustomer()
+        {
+            AddCustomerViewModel acVM = new AddCustomerViewModel();
+                
+            acVM.Customers = _context.Customers.ToList();
+
+            return View(acVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCustomer(AddCustomerViewModel acVM)
+        {
+            var customer = new Customer()
+            {
+                //CustomerId = acVM.CustomerId,
+                FirstName = acVM.FirstName,
+                LastName = acVM.LastName,
+                PhoneNumber = acVM.PhoneNumber,
+                Address = acVM.Address,
+                ZipCode = acVM.ZipCode,
+                City = acVM.City,
+                Country = acVM.Country,
+                Email = acVM.Email,
+
+            };
+
+            await _context.Customers.AddAsync(customer);
+            _context.SaveChanges();
+
+
+            return RedirectToAction("AddCustomer");
+        }
+
+        [HttpGet]
+        public IActionResult EditCustomer(int id)
+        {
+            //var customer = await _context.Customers.FirstOrDefault(x =>x.CustomerId == id)
+            var customer = _context.Customers.FirstOrDefault(x => x.CustomerId == id);
+
+            if(customer != null)
+            {
+                var viewModel = new UpdateCustomerViewModel()
+                {
+                    CustomerId = customer.CustomerId,
+                    FirstName = customer.FirstName,
+                    LastName = customer.LastName,
+                    PhoneNumber = customer.PhoneNumber,
+                    Address = customer.Address,
+                    ZipCode = customer.ZipCode,
+                    City = customer.City,
+                    Country = customer.Country,
+                    Email = customer.Email,
+                };
+                return View("EditCustomer", viewModel);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult EditCustomer(UpdateCustomerViewModel model)
+        {
+            var customer = _context.Customers.Find(model.CustomerId);
+
+            if(customer != null)
+            {
+
+                customer.FirstName = model.FirstName;
+                customer.LastName = model.LastName;
+                customer.PhoneNumber = model.PhoneNumber;
+                customer.Address = model.Address;
+                customer.ZipCode = model.ZipCode;
+                customer.City = model.City;
+                customer.Country = model.Country;
+                customer.Email = model.Email;
+
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteCustomer(int id)
+        {
+            var customer = _context.Customers.FirstOrDefault(x => x.CustomerId == id);
+
+            if(customer != null)
+            {
+                _context.Customers.Remove(customer);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult CustomerDetails(int id)
+        {
+            var customer = new Customer();
+            customer = _context.Customers.FirstOrDefault(x => x.CustomerId == id);
+
+            return View(customer);
         }
     }
 }
---
+++ LexiconMVCEndProject/ViewModels/AddCustomerViewModel.cs
@@ -0,0 +1,28 @@
+﻿using LexiconMVCEndProject.Models;
+
+namespace LexiconMVCEndProject.ViewModels
+{
+    public class AddCustomerViewModel
+    {
+        //public int CustomerId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Address { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public string Email { get; set; }
+
+        public List<Customer> Customers { get; set; }
+
+    }
+}
