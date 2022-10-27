1bd481677eff6212b6d6ea7abd3e0b22a294965f
SimonEliassonSE <102283683+SimonEliassonSE@users.noreply.github.com>
2022-10-27 11:38:59 +0200

    Swagger & CORS added. Added 1 TestAPI (CustomerAPIController).

---
+++ LexiconMVCEndProject/Controllers/CustomerAPIController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LexiconMVCEndProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerAPIController : ControllerBase
+    {
+        // GET: api/<CustomerAPIController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<CustomerAPIController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CustomerAPIController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CustomerAPIController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CustomerAPIController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- LexiconMVCEndProject/Program.cs
+++ LexiconMVCEndProject/Program.cs
@@ -1,14 +1,34 @@
 using LexiconMVCEndProject.Data;
 using Microsoft.EntityFrameworkCore;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+
+                        builder =>
+                        {
+                            builder.WithOrigins("*")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                        });
+
+});
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddMvc();
+builder.Services.AddControllers();
+builder.Services.AddRazorPages();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddSession(options =>
 {
@@ -24,9 +44,15 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseCors(MyAllowSpecificOrigins);
+app.UseSwaggerUI();
+app.UseSession();
+app.MapControllers();
+app.MapRazorPages();
+app.UseSwagger(x => x.SerializeAsV2 = true);
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
 
 app.UseRouting();
 
