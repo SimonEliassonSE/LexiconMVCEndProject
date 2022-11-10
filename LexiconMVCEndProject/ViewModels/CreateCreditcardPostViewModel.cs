45e3a532c790c929667977616025d3031c20fcd8
SimonEliassonSE <102283683+SimonEliassonSE@users.noreply.github.com>
2022-11-10 20:14:53 +0100

    Added Connections between frontend(react) and backend(ASP.Net Mvc).

---
+++ LexiconMVCEndProject/ViewModels/CreateCreditcardPostViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace LexiconMVCEndProject.ViewModels
+{
+    public class CreateCreditcardPostViewModel
+    {
+        public string CreditNumber { get; set; }
+        public string CCV { get; set; }
+        public string Bank { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
